c5c9c782570f29e38f0d54314b316973d7c774ba
eli-silver <eli_silver@brown.edu>
2023-02-15 11:49:16 -0500

    manually changed code to most recent changes, as captured in branch shaker_as_main

--- Shaker_Control/Shaker.py
+++ Shaker_Control/Shaker.py
@@ -7,8 +7,13 @@ Created on Tue Dec  6 14:45:10 2022
 """
 
 from Tone import Tone
-import pygame
 from Window import Window
+from Plot import Plot
+from Serial_Monitor import Serial_Monitor
+from Filter import Filter
+
+import pygame
+import pygame_gui
 from time import sleep
 import threading
 import serial
@@ -16,7 +21,6 @@ import numpy as np
 
 bits = 16
 sample_rate = 44100
-#pygame.mixer.pre_init(sample_rate,bits)
 
 
 class Shaker:
@@ -24,7 +28,85 @@ class Shaker:
     def __init__(self):
         self.game = pygame.init()
         pygame.mixer.init(sample_rate,bits)
-        self.tone = self.load_tone(200,0.25,speaker=None)
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display
+
+        self.init_tone()
+        self.init_window()
+        self.init_plot()
+        self.init_serial_monitor()
+        self.run_serial_monitor = False
+        if self.run_serial_monitor:
+            self.serial_monitor.serial_input_background_init()
+        self.init_filter()
+
+        self.total_time = 0
+
+        ### begin update loop ###
+        self.run = True
+        self.update_loop()
+
+
+    def update_loop(self):
+        while self.run:
+            time_delta = self.clock.tick(120)/1000
+
+            for event in pygame.event.get():
+                self.window_ui_manager.process_events(event)
+
+                # close window if clicked (x)
+                if event.type == pygame.QUIT:
+                    self.run = False
+                    if self.run_serial_monitor:
+                        self.serial_monitor.close()
+                    self.exit()
+
+                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#frequency_input'):
+                    self.set_tone_frequency(float(event.text))    
+                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#volume_input'):
+                    self.set_tone_volume(float(event.text)/100.0)
+                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#seq_path_input'):
+                    self.path = event.text
+                    self.load_tone_sequence(self.path)
+
+
+                if (event.type == pygame_gui.UI_BUTTON_PRESSED):
+                    #print('button pressed')
+                    if (event.ui_element == self.window.button_play):
+                        print('Play Pressed')
+                        self.play_tone()
+                    if (event.ui_element == self.window.button_pause):
+                        print('Pause Pressed')
+                        self.pause_tone()
+                    
+                    if (event.ui_element == self.window.button_play_seq):
+                        print('Play Sequence Pressed')
+                        self.play_sequence()
+
+            if self.run_serial_monitor:
+                self.update_serial_monitor(time_delta)
+            self.window_ui_manager.update( time_delta )
+            self.plot.update( time_delta )
+            self.window_ui_manager.draw_ui(self.window_screen)
+            pygame.display.update() 
+        pygame.quit()
+
+    
+    
+    
+    
+    def update_serial_monitor(self, time_delta):
+        self.total_time += time_delta
+
+        if self.serial_monitor.buff_is_ready():
+            data = self.serial_monitor.get_buffer()
+            self.plot.add_point('ax', self.total_time, data[0][0])
+            self.plot.add_point('ay', self.total_time, data[0][1])
+            self.plot.add_point('az', self.total_time, data[0][2])
+    
+    
+    def init_tone(self):
+        self.tone = self.load_tone(220,0.25,speaker=None)
         self.tone_list = []
         self.tone_running = False
         self.tone_sequence_running = False
@@ -32,12 +114,25 @@ class Shaker:
         self.tone_sequence = None
         self.fade_ms = 0
         self.speaker = 'r'
-        self.total_time = 0
 
-        self.window = Window(self)
+    def init_window(self):
+        self.window = Window(self.display)
+        self.window_plot_area = self.window.get_plot_area()
+        self.window_display = self.window.get_display()
+        self.window_screen = self.window.get_screen()
+        self.window_ui_manager = self.window.get_ui_manager()
 
-    def end_game(self):
-        print('GAME OVER')
+    def init_plot(self):
+        self.plot = Plot(self.window_screen, *self.window_plot_area )
+
+    def init_serial_monitor(self):
+        self.serial_monitor = Serial_Monitor(num_data_bytes=2, num_traces = 4)
+
+    def init_filter(self):
+        pass
+
+    def exit(self):
+        print('THANKS FOR SHAKING')
     
     def load_tone(self, freq, amp, speaker=None, fade_ms=0):
         return Tone(freq, amp, speaker, fade_ms)
@@ -96,14 +191,6 @@ class Shaker:
         if(volume < 0):
             volume=0
         self.tone.set_volume(volume)
-        #frequency = self.tone.get_frequency()
-        #next_tone = self.load_tone(frequency, volume, speaker=None, fade_ms=0)
-        #if self.tone_running:
-        #    self.pause_tone()
-        #    self.tone=next_tone
-        #   self.play_tone()
-        #else:
-        #    self.tone=next_tone
         
     def clear_tone_list(self):
         for tone in self.tone_list:
--- Shaker_Control/Window.py
+++ Shaker_Control/Window.py
@@ -5,97 +5,99 @@ Created on Tue Dec  6 16:26:04 2022
 
 @author: eli
 """
-from Plot import Plot
-from Serial_Monitor import Serial_Monitor
-from Filter import Filter
 import pygame
 import pygame_gui
 
 WIDTH, HEIGHT = 1000, 500
 
+
 class Window:
     
-    def __init__(self, shaker):
-        self.window = pygame.display
-        self.running = True
-        self.shaker = shaker
+    def __init__(self, display):
+        self.display = display #pygame.display
         self.setup_ui()
         self.plot_area = (350,50,600,400)
-        self.plot = Plot(self.screen, *self.plot_area )
-        self.path = ''
-        self.total_time = 0
-        #self.filter = Filter()
-
-        self.get_serial_input = True
-        if self.get_serial_input:
-            self.serial_monitor = Serial_Monitor(num_data_bytes = 2, num_traces=4)
-            self.serial_monitor.serial_input_background_init()
-        self.loop()
+        #self.path = ''
+        #self.get_serial_input = False
+        #if self.get_serial_input:
+        #    self.serial_monitor = Serial_Monitor(num_data_bytes = 2, num_traces=4)
+        #    self.serial_monitor.serial_input_background_init()
+        #self.loop()
 
     
-    def loop(self):    
-        while self.running:
-            # get user input
-            time_delta = self.clock.tick(120)/1000.0
+    # def loop(self):    
+    #     while self.running:
+    #         # get user input
+    #         #time_delta = self.clock.tick(120)/1000.0
             
-            for event in pygame.event.get():
-                # close window if clicked (x)
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    if self.get_serial_input:
-                        self.serial_monitor.close()
-                    self.shaker.end_game()
+    #         for event in pygame.event.get():
+    #             # close window if clicked (x)
+    #             if event.type == pygame.QUIT:
+    #                 self.running = False
+    #                 if self.get_serial_input:
+    #                     self.serial_monitor.close()
+    #                 self.shaker.end_game()
                             
-                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#frequency_input'):
-                    self.shaker.set_tone_frequency(float(event.text))    
-                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#volume_input'):
-                    self.shaker.set_tone_volume(float(event.text)/100.0)
-                if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#seq_path_input'):
-                    self.path = event.text
-                    self.shaker.load_tone_sequence(self.path)
+    #             if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#frequency_input'):
+    #                 self.shaker.set_tone_frequency(float(event.text))    
+    #             if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#volume_input'):
+    #                 self.shaker.set_tone_volume(float(event.text)/100.0)
+    #             if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#seq_path_input'):
+    #                 self.path = event.text
+    #                 self.shaker.load_tone_sequence(self.path)
                 
                 
-                if (event.type == pygame_gui.UI_BUTTON_PRESSED):
-                    #print('button pressed')
-                    if (event.ui_element == self.button_play):
-                        print('Play Pressed')
-                        self.shaker.play_tone()
+    #             if (event.type == pygame_gui.UI_BUTTON_PRESSED):
+    #                 #print('button pressed')
+    #                 if (event.ui_element == self.button_play):
+    #                     print('Play Pressed')
+    #                     self.shaker.play_tone()
                 
-                    if (event.ui_element == self.button_pause):
-                        print('Pause Pressed')
-                        self.shaker.pause_tone()
+    #                 if (event.ui_element == self.button_pause):
+    #                     print('Pause Pressed')
+    #                     self.shaker.pause_tone()
                     
-                    if (event.ui_element == self.button_play_seq):
-                        print('Play Sequence Pressed')
-                        self.shaker.play_sequence()
+    #                 if (event.ui_element == self.button_play_seq):
+    #                     print('Play Sequence Pressed')
+    #                     self.shaker.play_sequence()
                 
                 
-                self.ui_manager.process_events(event)
-            if self.get_serial_input:
-                self.update_serial_monitor(time_delta)
-            self.ui_manager.update( time_delta )
-            self.plot.update( time_delta )
-            self.ui_manager.draw_ui(self.screen)
-            pygame.display.update()
+    #             self.ui_manager.process_events(event)
+    #         if self.get_serial_input:
+    #             self.update_serial_monitor(time_delta)
+    #         self.ui_manager.update( time_delta )
+    #         self.plot.update( time_delta )
+    #         self.ui_manager.draw_ui(self.screen)
+    #         pygame.display.update()
         
-        #Done! Time to quit.
-        pygame.quit()
+    #     #Done! Time to quit.
         
 
-    def update_serial_monitor(self, time_delta):
-        self.total_time += time_delta
+    # def update_serial_monitor(self, time_delta):
+    #     self.total_time += time_delta
 
-        if self.serial_monitor.buff_is_ready():
-            data = self.serial_monitor.get_buffer()
-            self.plot.add_point('ax', self.total_time, data[0][0])
-            self.plot.add_point('ay', self.total_time, data[0][1])
-            self.plot.add_point('az', self.total_time, data[0][2])
+    #     if self.serial_monitor.buff_is_ready():
+    #         data = self.serial_monitor.get_buffer()
+    #         self.plot.add_point('ax', self.total_time, data[0][0])
+    #         self.plot.add_point('ay', self.total_time, data[0][1])
+    #         self.plot.add_point('az', self.total_time, data[0][2])
+    
+    def get_plot_area(self):
+        return self.plot_area
+    
+    def get_screen(self):
+        return self.screen
+
+    def get_ui_manager(self):
+        return self.ui_manager
+    
+    def get_display(self):
+        return self.display
 
     def setup_ui(self):
-        self.screen = self.window.set_mode([WIDTH, HEIGHT])
+        self.screen = self.display.set_mode([WIDTH, HEIGHT])
         self.screen.fill("white")
-        self.window.set_caption('Harris Lab Shaker Control')
-        self.clock = pygame.time.Clock()
+        self.display.set_caption('Harris Lab Shaker Control')
         self.ui_manager = pygame_gui.UIManager((WIDTH,HEIGHT))
        
         self.freq_input_line = pygame_gui.elements.UITextEntryLine(relative_rect=((50,100),(250,40)), manager=self.ui_manager, object_id='#frequency_input' )
